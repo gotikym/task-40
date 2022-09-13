753c0aaf5962b2d3fc99b18a2ca4dc314e1a2242
gotikym <gotikym9@mail.ru>
2022-09-13 21:02:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Draw draw = new Draw();
+        Player player = new Player(7, 7);
+
+        draw.DrawPlayer(player.X, player.Y);
+    }
+}
+
+class Player
+{
+    public int X { get; private set; }
+    public int Y { get; private set; }
+
+    public Player(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
+class Draw
+{
+    public void DrawPlayer(int x, int y, char simbol = 'Ж')
+    {
+        Console.SetCursorPosition(x, y);
+        Console.Write(simbol);
+    }
+}
